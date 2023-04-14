1013395f38651bc17ebc529a1e67697e617fef00
Kevin Benny <kbenny5025@stu.d214.oeg>
2023-04-14 13:33:11 -0500

    yurrrrrr

--- ContentView.swift
+++ ContentView.swift
@@ -6,13 +6,21 @@ struct ContentView: View {
     @State var answer:String
     var body: some View {
         VStack{
-          TextField("first", value: $new, formatter: NumberFormatter())
+            TextField("first", value: $new, formatter: NumberFormatter())
             TextField("first", value: $new2, formatter: NumberFormatter())
-        VStack {
             Button("Multiply"){
                 
             }
+            Button("Add"){
+                
+            }
+            Button("Subtract"){
+                
+            }
+            Button("Divide"){
+                
+            }
         }
-       
+        
     }
 }
