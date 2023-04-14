c4b65b8867127c502c04baf82b26c1e60d30cf89
Kevin Benny <kbenny5025@stu.d214.oeg>
2023-04-14 13:21:42 -0500

    my nanny

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answer:String
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Multiply"){
+                
+            }
         }
     }
 }
